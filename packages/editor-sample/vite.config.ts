71d8485e04ad2d7ab6e47e082d66cf46c53ac806
Nguyễn Văn Đức <133721410+ducnguyen1002@users.noreply.github.com>
2025-09-26 13:42:39 +0700

    update server config

--- packages/editor-sample/vite.config.ts
+++ packages/editor-sample/vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react-swc';
 
+const ALLOWED_HOST = import.meta.env.VITE_ALLOWED_HOST;
+
 export default defineConfig({
   plugins: [react()],
   base: '/email-builder-js/',
+  server: {
+    allowedHosts: [ALLOWED_HOST],
+  },
 });
